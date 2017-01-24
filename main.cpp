f3737864530fdc743eb1f2d4e935e5a0092f070d
digital080 <saqensealy@hotmail.com>
2017-01-24 11:49:15 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <string.h>
+#include <string>
 #include "test.h"
 #include <vector>
 using namespace std;
@@ -7,19 +7,21 @@ using namespace std;
 int main (){
 	
 	char xChar = 'c';
-	string string1 = "hello";
+	string string1 = "";
 	int xnum1 = 0;
 	int xnum2 = 0;
 	char xChar2 = 'p';
+	char xChar3 = '\0';
 	Test testObj; // object
 	Test testObj2;
 	Test testObj3;
 	Test testObj4;
 	vector <Test> objVect; 
 	
-	cout << "Enter 2 characters and a string: " << endl;
+	cout << "Enter 3 characters and a string: " << endl;
 	cin >> xChar;
 	cin >> xChar2;
+	cin >> xChar3;
 	cin >> string1;
 	
 	
@@ -29,23 +31,27 @@ int main (){
 	//testObj.setVect(xChar, string1, xnum1, xnum2);
 	//objVect.push_back(testObj);
 	
-	testObj.setString(string1);
+	testObj.setChar(xChar3);
 	testObj2.setChar(xChar);
 	testObj3.setVect(xChar, string1);
 	testObj4.setChar(xChar2);
 	
-	objVect.push_back(testObj3);
 	objVect.push_back(testObj);
+	objVect.push_back(testObj3);
+	
 	objVect.push_back(testObj2);
 	objVect.push_back(testObj4);
 	
 
 	
 	for(int x = 0; x < objVect.size(); x++) {
-		cout << objVect[x].getString() << objVect[x].getChar() << endl;
 		
+		cout << objVect[x].getString();
 		
-		
+
+		cout << objVect[x].getChar() << endl;
+	
+	
 		}
 	
 	}
