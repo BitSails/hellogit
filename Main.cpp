dd0b444d94cc1c61535036faac37128c1a87db3d
Skyrain25 <shontellebarker@gmail.com>
2017-01-25 11:57:24 -0400

    Added client file

---
+++ Main.cpp
@@ -0,0 +1,32 @@
+#include "Account.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main( )
+{
+	Account myAccount;
+	vector <Account> acctList;
+	
+	   myAccount.setName ("Bob");
+	   myAccount.setNum (12);
+	acctList.push_back (myAccount);
+	  myAccount.setName ("Tim");
+  	   myAccount.setNum (10);
+	acctList.push_back(myAccount);
+	   myAccount.setName ("Rich");
+	   myAccount.setNum (35);
+	acctList.push_back (myAccount);
+	   myAccount.setName ("Chris");
+	   myAccount.setNum (66);
+	acctList.push_back (myAccount);
+
+	for (int i = 0;  i < acctList.size ( );   i++)
+	 {
+		cout << acctList[i].getName()<< "" << acctList[i].getNum ();
+		
+	 }
+
+}
+
