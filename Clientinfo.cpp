017268a668cc7852b93e123ddb0c8f775e98f2e0
scotty1246 <scotty1246@users.noreply.github.com>
2017-01-25 12:11:41 -0400

    Clientinfo.cpp

---
+++ Clientinfo.cpp
@@ -0,0 +1,21 @@
+#include "Client.h"
+
+void Clientinfo::setname (std::string clientname)
+{
+   name = clientname;
+}
+
+std::string Clientinfo::getname ()
+{
+   return name;
+}
+
+void Clientinfo::setage (int clientage)
+{
+   age = clientage;
+}
+
+int Clientinfo::getage ()
+{
+   return age;
+}
