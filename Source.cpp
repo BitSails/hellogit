df661be39649ebe2a667468873893cf43a2dea92
DoubleLift3 <shaquone_10@hotmail.com>
2017-01-25 10:59:45 -0400

    Source.cpp

---
+++ Source.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "Header.h" 
+using namespace std;
+
+int main(){
+int number; 
+
+student myStudent;
+vector<student> myVector;
+cout <<"Enter number";
+cin>>number;
+/*
+myVector.push_back(number);
+*/
+}
+
