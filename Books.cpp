187d40b46d1ba338c5b00e2d3d3d82951b9822d3
blvckk <blvckk@users.noreply.github.com>
2017-01-26 21:49:24 +0000

    Added header & Implementation file

---
+++ Books.cpp
@@ -0,0 +1,18 @@
+#Include "Books.h"
+
+void book::setbName(string x)
+{
+bName = x;
+}
+stringbook:: getbName()
+{
+return bName;
+}
+void book:: setprice (double y)
+price =y;
+}
+double book:: getprice()
+{
+return price;
+}
+
---
+++ Books.h
@@ -0,0 +1,19 @@
+#Include <iostream>
+#Include < string>
+using namespace std;
+
+class books
+{ 
+private:
+string bName;
+double price;
+
+public: 
+books();
+void setbName(string);
+string getbName ();
+void setprice (double);
+double getprice();
+};
+
+
