80a3dd95a79c68305ee9ba8a58c164aa6601b7d7
catkitcat <issabicobham@hotmail.com>
2017-01-26 14:52:36 -0400

    Added header file

---
+++ Cat.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vector>
+#include "Cat.h"
+using namespace std;
+
+int main()
+{
+	vector<Cat>cats
---
+++ Cat.h
@@ -0,0 +1,24 @@
+#include <string>
+using namespace std;
+class Cat{
+	public:
+		Cat(){
+			name=" ";
+			age=0.00;
+		}
+		void setName (string catN){
+			name=catN;
+		}
+		void setAge (float catAge){
+			age=catAge;
+		}
+		string getName(){
+			return name;
+		}
+		float getAge(){
+			return age;
+		}
+	private:
+		string name;
+		float age;
+};
