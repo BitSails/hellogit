85ceccc496821ddb29412ef336abc59a03d115cc
catkitcat <issabicobham@hotmail.com>
2017-01-26 15:30:01 -0400

    Added cpp file

--- Cat.cpp
+++ Cat.cpp
@@ -5,4 +5,24 @@ using namespace std;
 
 int main()
 {
-	vector<Cat>cats
+	Cat cats;
+	vector<Cat>catVec;
+	string catName;
+	int catAge;
+
+	for(int i=0;i<=3;i++)
+	{
+		cout<<"Please enter cat name and age \n";
+		cin>> catName >> catAge;
+		cats.setName(catName);
+		cats.setAge(catAge);
+		catVec.push_back(cats);
+	}
+
+	for(int i=0;i<=3;i++)
+	{
+		cout<<catVec[i].getName()<<"\t";
+		cout<<catVec[i].getAge()<<"\n";
+	}
+	return 0;
+}
