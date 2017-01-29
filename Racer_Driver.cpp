b1b64aa29a3b7730a13aa1e4dbcdd3e6de034e7a
MasterT1 <andre.brathwaite5@mycavehill.uwi.edu>
2017-01-29 17:25:28 -0400

    Revamped of the Driver file

--- Racer_Driver.cpp
+++ Racer_Driver.cpp
@@ -1,32 +1,74 @@
 //Racer Driver file
 #include<string>
-#include<array>
+#include<vector>
 #include<iostream>
 #include<iomanip>
+#include <algorithm>
 
 using namespace std;
 
 #include "Racer.h"
 
+void fillVector(vector<Racer>&);
+void printVector(const vector<Racer>&);
+//void printVector2(const vector<Racer>&);
+
+
 int main()
-{
-	string name = "";
-	int category = 0;
-	Racer bikeRacer(name, category);	
+{	
+	vector<Racer> racerArray;
 
-	cout << "Enter the Bike racer's name: ";
-	getline(cin, name);
+	fillVector(racerArray);
 
-	cout << "\nEnter the Bike racer's Category Level:";
-	cin >> category;
-
-	bikeRacer.setRacerName(name);
-	bikeRacer.setRacerCategory(category);	
-
-	bikeRacer.displayInfo();
+	printVector(racerArray);
+	
+	system("pause");
 
 	return 0;
 
 } 
 
-// Figured out how to get this done on the windows side of things using Visual Studio
+void fillVector(vector<Racer>& newRacerArray)
+{
+	for (int x = 0; x < 4; x++)
+	{
+		int category;
+		string name;
+		cout << "Enter the Bike racer's name: ";
+		getline(cin, name);
+
+		cout << "\nEnter the Bike racer's Category Level:";
+		cin >> category;
+		cin.ignore();
+
+		Racer newRacer(name, category);
+		newRacerArray.push_back(newRacer);
+		cout << endl;
+	}
+	cout << endl;
+}
+
+void printVector(const vector<Racer>& newprintRacer)
+{
+	unsigned int vectorSize = newprintRacer.size();
+
+	for (unsigned int i = 0; i < vectorSize; i++)
+	{
+		cout << "Rider's name: " << newprintRacer[i].getRacerName() << endl;
+		cout << "Rider's category: " << newprintRacer[i].getracerCategory() << endl;
+		cout << endl;
+	}
+
+}
+
+/*void printVector2(const vector<Racer>& newprintRacer)
+{
+	unsigned int vectorSize = newprintRacer.size();
+
+	for (unsigned int i = 0; i < vectorSize; i++)
+	{
+		cout << newprintRacer[i].displayInfo() << endl;
+	}
+
+}
+*/
