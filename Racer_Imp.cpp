28575c2cf99c27d5f36a57066a111fc50aa88c5e
MasterT1 <andre.brathwaite5@mycavehill.uwi.edu>
2017-01-29 17:32:32 -0400

    revamped with correct const

--- Racer_Imp.cpp
+++ Racer_Imp.cpp
@@ -5,11 +5,18 @@
 using namespace std;
 
 //constructor
-Racer::Racer(const string name, const int category)
+Racer::Racer()
 	{
-		racerName = name;
-		racerCategory = category;
-	}
+		racerName = " ";
+		racerCategory = 0;
+}
+
+//Overload Constructors
+Racer::Racer(string name, int category)
+{
+	racerName = name;
+	racerCategory = category;
+}
 
 //set the Racer name
 void Racer::setRacerName(string name)
@@ -18,7 +25,7 @@ void Racer::setRacerName(string name)
 }
 
 //retrieve the Racer name
-string Racer::getRacerName()
+string Racer::getRacerName() const
 {
 	return racerName;
 }
@@ -30,13 +37,13 @@ void Racer::setRacerCategory(int category)
 }
 
 //retrieve the Racer Category
-int Racer::getracerCategory()
+int Racer::getracerCategory() const
 {
 	return racerCategory;
 }
 
 //displays all of the Racer object's data
-void Racer::displayInfo()
+void Racer::displayInfo() const
 {
 	cout << "**** Rider's Name and Category	****" << endl;
 	cout << "Name: " << racerName << endl;
