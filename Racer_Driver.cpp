17886ac5c1879d78313163c7f4ebb028c5eb07ee
MasterT1 <andre.brathwaite5@mycavehill.uwi.edu>
2017-01-29 11:16:16 -0400

    Figured out how to used the class implementation structure

--- Racer_Driver.cpp
+++ Racer_Driver.cpp
@@ -3,6 +3,7 @@
 #include<array>
 #include<iostream>
 #include<iomanip>
+
 using namespace std;
 
 #include "Racer.h"
@@ -11,19 +12,21 @@ int main()
 {
 	string name = "";
 	int category = 0;
-	Racer bikeRacer(name, category);
-	
+	Racer bikeRacer(name, category);	
+
 	cout << "Enter the Bike racer's name: ";
 	getline(cin, name);
+
 	cout << "\nEnter the Bike racer's Category Level:";
 	cin >> category;
 
 	bikeRacer.setRacerName(name);
-	bikeRacer.setRacerCategory(category);
-	
+	bikeRacer.setRacerCategory(category);	
+
 	bikeRacer.displayInfo();
 
 	return 0;
+
 } 
 
-
+// Figured out how to get this done on the windows side of things using Visual Studio
