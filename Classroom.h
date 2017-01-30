2822192c3254026edc5e5d990c50cff360968990
RainCoder <jyolainnie_daniel@hotmail.com>
2017-01-29 19:21:31 -0500

    Added using namespace std;

--- Classroom.h
+++ Classroom.h
@@ -1,16 +1,17 @@
 # include <iostream>
 # include <string>
+using namespace std;
 
 class Classroom{
 public:
   Classroom();
   void setSeats(int x);
   int getSeats();
-  void setLocation(std::string y);
-  std::string getLocation();
+  void setLocation(string y);
+  string getLocation();
 
 private:
   int seats;
-  std::string location;
+  string location;
 
 };
