4b39924790ab3c67aaca6f8f9bf0d89ba4b63b93
WarSloth21 <WarSloth21@users.noreply.github.com>
2017-02-05 19:53:41 -0400

    Updated client file

--- Main.cpp
+++ Main.cpp
@@ -7,18 +7,33 @@ using namespace std;
 
 void fillVector(vector<Student> newClass) ;
 void printVector(const vector<Student> newClass);
+int linearSearch(vector<Student> newClass, auto key);
 
 int main() {
 
 	vector<Student> myclass;
-
+	
 	fillVector(myclass);
 	printVector(myclass) ;
-
+	linearSearch(myclass,  key);
 	
 	return 0;
 	
 }
+
+int linearSearch(vector<Student> newClass, auto key)
+	{
+		string key;
+		for(int i = 0; i < newClass.size(); i ++)
+		{
+			if (newClass[i] == key)//we found it
+			{
+				return i;//return its location
+			}
+		}//end for
+		return -1;//element not found
+	}
+
 
 void fillVector(vector<Student> newClass) {
 
