ae730473ecb222016c72bad534b98059f7677e7d
catkitcat <catkitcat@users.noreply.github.com>
2017-02-06 11:41:26 -0400

    added to cat.pp

--- Cat.cpp
+++ Cat.cpp
@@ -3,6 +3,18 @@
 #include "Cat.h"
 using namespace std;
 
+int linearSearch(auto data, auto key)//prototype
+{	
+	for (int i=0; i < data.size(); i++)
+	{
+		if(data[i]==key)
+		{
+			return i;
+		}
+	}
+	return -1;
+}
+
 int main()
 {
 	Cat cats;
@@ -25,4 +37,56 @@ int main()
 		cout<<catVec[i].getAge()<<"\n";
 	}
 	return 0;
+	
+	
+  vector<string> inputs;
+  string search_key, input;
+  int result;
+
+   cout<<"Welcome to \"search it\". We first need some input data."<<endl;
+   cout<<"We'll assume the inputs do not have any spaces."<<endl<<endl;
+   cout<<"To end input type the #-character (followed by Enter)"<<endl<<endl;
+
+   cin>>input;
+ 
+    while(input != "#")//read an unknown number of inputs from keyboard
+    {
+       inputs.push_back(input);
+       cin>>input;
+    }
+
+   cout<<endl<<"["<<inputs.size()<<" values read from input source]"<<endl<<endl;
+
+  if(inputs.size() == 0)//no input
+  {
+      cout<<endl<<"No input received, quiting..."<<endl<<endl;
+       exit(1);//nothing to do but quit program
+  }
+ 
+   cout<<endl<<"To end input type the #-character (followed by Enter)"<<endl<<endl;
+  cout<<"Enter a value to search for: ";
+
+
+   cin>>search_key;
+ 
+    while(search_key != "#")//perform searches until sentinel entered
+    {
+        result = linearSearch(inputs,search_key);
+
+        cout<<"  '"<<search_key<<"' was ";
+
+        if (result == -1)
+          cout<<"not found";
+        else
+          cout<<"found at index "<<result;
+
+
+        cout<<endl<<endl<<"Enter a value to search for: ";
+        cin>>search_key; 
+    }
+
+   cout<<endl<<"Program \"search it\" is now finished."<<endl<<endl;
+
+    return 0;
 }
+
