d64f59340dc14e3690bd9cbfda95426d24c9ca5e
metrojr <metrojr@users.noreply.github.com>
2017-02-07 11:32:51 -0400

    Added search

--- client.cpp
+++ client.cpp
@@ -2,6 +2,7 @@
 #include<vector>
 #include<iostream>
 using namespace std;
+int linearSearch(auto data,auto key);
 int main()
 {
 LabOne ob1;
@@ -20,9 +21,21 @@ ob1.setDeci(12);
 Vec.push_back(ob1);
 cout<<Vec.size()<<endl;
 for(int i=0;i<Vec.size();i++){
-cout<<Vec.[i].getNum()<<"."<<Vec[i].getDeci()<<endl;
+cout<<Vec[i].getNum()<<"."<<Vec[i].getDeci()<<endl;
 }
 system("pause");
 return 0;
 }
-
+int linearSearch(auto data,auto num)
+{
+for(int i=0;i<data.size();i++)
+{
+cout<<"Enter value to search"<<endl;
+if(data[i].getNum()==num)
+{
+cout<<"Value has been found"<<endl;
+return i;
+}
+}
+return -1;
+}
