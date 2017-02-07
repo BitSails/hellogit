1891e5ec7faa10f54f20f913c69f710594034238
MasterT1 <andre.brathwaite5@mycavehill.uwi.edu>
2017-02-07 11:37:13 -0400

    Lab3 linear function added

--- Racer_Driver.cpp
+++ Racer_Driver.cpp
@@ -1,9 +1,10 @@
 //Racer Driver file
-#include<string>
-#include<vector>
-#include<iostream>
-#include<iomanip>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <iomanip>
 #include <algorithm>
+#include <cstdlib>//for "exit()" on some systems
 
 using namespace std;
 
@@ -12,15 +13,44 @@ using namespace std;
 void fillVector(vector<Racer>&);
 void printVector(const vector<Racer>&);
 void printVector2(const vector<Racer>&); //Need help to understand this.
+int linearSearch(vector<Racer>& data, auto key);//function prototype for linear search.
 
 
 int main()
 {	
+	string search_key, input;
+	int result;	
 	vector<Racer> racerArray;
 
 	fillVector(racerArray);
+	
+	
+	// Implemented the linear function search from the algos lab.
+	// Commit out from here to "printVector2" to gain back the orginal 
+	//implementation of this program.
+	
+	cout<<endl<<"To end input type the #-character (followed by Enter)"<<endl<<endl;
+	cout<<"Enter a value to search for: ";
+	cin>>search_key;
+	
+	
+	while(search_key != "#")//perform searches until sentinel entered
+    {
+        result = linearSearch(racerArray,search_key);
 
-	printVector2(racerArray);
+        cout<<"  '"<<search_key<<"' was ";
+
+        if (result == -1)
+          cout<<"not found";
+        else
+          cout<<"found at index "<<result;
+
+
+        cout<<endl<<endl<<"Enter a value to search for: ";
+        cin>>search_key; 
+    }
+
+	//printVector2(racerArray);
 	
 	system("pause");
 
@@ -70,5 +100,18 @@ void printVector2(const vector<Racer>& newprintRacer)
 		newprintRacer[i].displayInfo();
 	}
 
+}
+
+int linearSearch(vector<Racer>& data, auto key)
+{	
+	for (int i = 0; i < data.size(); i++)
+	{
+		if (data[i].getRacerName() == key)
+		{
+			return i;
+			}
+		}
+	return -1;
+	
 }
 
