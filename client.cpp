44c71df66b1fca2570e9a28739380534fb0c4341
metrojr <metrojr@users.noreply.github.com>
2017-02-07 12:05:02 -0400

    Changes

--- client.cpp
+++ client.cpp
@@ -7,7 +7,6 @@ int main()
 {
 LabOne ob1;
 vector<LabOne>Vec;
-int num,key;
 ob1.setNum(1);
 ob1.setDeci(54);
 Vec.push_back(ob1);
@@ -24,21 +23,11 @@ cout<<Vec.size()<<endl;
 for(int i=0;i<Vec.size();i++){
 cout<<Vec[i].getNum()<<"."<<Vec[i].getDeci()<<endl;
 }
-cout<<"Enter a value to be searched"<<endl;
-cin>>num;
-while(key != 0)
-{
-num=linearSearch(data,key);
-cout<<" "<<key<<"was";
-if(num == -1)
-cout<<"not found";
-else
-cout<<"found at index"<<num;
 }
 system("pause");
 return 0;
 }
-int linearSearch(auto data,auto key)
+int linearSearch(auto data,auto num)
 {
 for(int i=0;i<data.size();i++)
 {
