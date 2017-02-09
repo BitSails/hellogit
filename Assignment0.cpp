1897ad7c04122ad53fb21c895e91fd462839f963
NutFactory <thatshaley@hotmail.com>
2017-02-08 22:04:51 -0500

    Added Linear Function

--- Assignment0.cpp
+++ Assignment0.cpp
@@ -7,12 +7,36 @@ using namespace std;
 
 void SquareAndAddition(vector<NumberManip>&);
 void printnumbers(vector<NumberManip>&);
+int linearSearch(auto data, double key);
 
 int main() {
+	
+	double search_key;
+	int result;
 	
 	vector<NumberManip> NumManip;
 	SquareAndAddition(NumManip);
 	printnumbers(NumManip);
+	
+	cout<<"Enter a value to search for, enter -1 to exit.";
+
+   cin>>search_key;
+ 
+    while(search_key != -1)//perform searches until sentinel entered
+    {
+        result = linearSearch(NumManip,search_key);
+
+        cout<<"  '"<<search_key<<"' was ";
+
+        if (result == -1)
+          cout<<"not found";
+        else
+          cout<<"found at index "<<result;
+
+
+        cout<<endl<<endl<<"Enter a value to search for: ";
+        cin>>search_key; 
+    }
 	return 0;
 
 }
@@ -39,3 +63,16 @@ void printnumbers(vector<NumberManip>& ManipNumber) {
 		
 	}
 }
+
+int linearSearch(auto Data, double key)
+{
+	for(int i = 0; i < Data.size(); i++)
+	{
+		if (Data[i].getAddition() == key)
+		{
+			return i;
+		}
+	}	
+	return -1;
+}
+
