cd9f08d38add27c5f93cbb627ac476eb8be47e02
blvckk <blvckk@users.noreply.github.com>
2017-02-09 23:15:42 +0000

    Added Lab #3

--- client.cpp
+++ client.cpp
@@ -4,22 +4,74 @@
 #include <string>
 using namespace std;
 
+int linearSearch(auto data, auto key)   //prototype
+{
+	for (int i=0; i < data.size(); i++)
+	{
+		if (data[i].getbName() == key)
+		{ 
+			return i;
+		}
+	}	// end for
+	return -1;
+}
+
 int main()
 {
 vector <books> Books (4);
+string search_key;
 string bName;
 double price;
+int result;
+
 for (int i=0; i<4; i++)
 {
-cout << "Book Title:" << endl;
+cout << "Enter the Book Title:" << endl;
 cin >> bName;
-Books [i].setbName (bName);
-cout << "Price:" << endl;
-cin >> price;
-Books[i].setprice (price);
+ while(bName != "#")	//read an unknown number of inputs from keyboard
+    {
+       Books.push_back(books);
+       cin>>bName;
+    }
+    cout<<endl<<"["<<Books.size()<<" values read from input source]"<<endl<<endl;
+    
+//Books [i].setbName (bName);
+
+
+// QUIT
+if(Books.size() == 0)//no input
+  {
+      cout<<endl<<"No input received, quiting..."<<endl<<endl;
+       exit(1);//nothing to do but quit program
+  }
+
+// INSERT SERACH KEY
+  cout<<"Enter a value to search for: ";
+   cin>>search_key;
+   
+//COMPARE SEARCH KEY W/ VECTORS   
+while (search_key != "#")	// # of searches
+{
+	result = linearSearch (Books,search_key);		//vector books -> input bName
+
+        cout<<"  '"<<search_key<<"' was ";
+
+        if (result == -1)
+          cout<<"not found";
+        else
+          cout<<"found at index "<<result;
+          
+          
+cout<<endl<<endl<<"Enter a value to search for: ";
+        cin>>search_key; 
+        
+     //   cout << "Price:" << endl;
+//cin >> price;
+//Books[i].setprice (price);
 }
+    }
 cout << "Book Information: " << endl;
-for (int i=0; i<4; i++)
+  for (int i=0; i<4; i++)
 {
 bName= Books[i].getbName();
 price= Books[i].getprice();
@@ -27,3 +79,5 @@ cout << bName << " " << price << endl;
 }
 cin >> bName;
 }
+    
+
