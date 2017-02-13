9cd74302d4c74e32cae82a9ea4f49b388e9cfce7
WarSloth21 <WarSloth21@users.noreply.github.com>
2017-02-13 10:31:34 -0400

    Updated Client File

--- Main.cpp
+++ Main.cpp
@@ -46,7 +46,7 @@ int linearSearch(vector<Student> newClass, string name)
 		
 		for(int i = 0; i < newClass.size(); i ++)
 		{
-			if ( newClass[i].getName == name )//we found it
+			if ( newClass[i].getName() == name )//we found it
 			{
 				return i;//return its location
 			}
