e88b8adcc9ecceb016fd6ed13e84b10e52219d5b
Axum25 <josephbuffong@hotmail.com>
2017-02-23 15:45:00 -0400

    Lab #3
    Forgot to Upload Changes

--- GroceryStore.cpp
+++ GroceryStore.cpp
@@ -5,37 +5,49 @@
 #include "Fruits.h"
 #include <string>
 #include <vector>
+
 using namespace std;
+
+int linearSearch( auto data, auto key);
 
 int  main()
 {
 vector <Fruits> Bags;
 
-cout << "Bags needed four (4)"<<endl;
 
-	string choice;
+cout << "Bags needed four (4)"<<endl<<endl;
+
+	string choice, search_key;
 	double quantity;
+	int result;
 
-	cout << "Enter Fruit Name";
+	cout << "Enter Fruit Name: ";
 		cin >> choice;
-	cout << "Enter Total Weight";
+	cout << "Enter Total Weight: ";
 		cin >> quantity;
+
+		cout << ""<<endl;
 
 	Fruits Bag1(choice, quantity);
 	Bags.push_back(Bag1);
 
-	cout << "Enter Fruit Name";
+	cout << "Enter Fruit Name: ";
 			cin >> choice;
-		cout << "Enter Total Weight";
+
+		cout << "Enter Total Weight: ";
 			cin >> quantity;
+
+			cout << ""<<endl;
 
 		Fruits Bag2(choice, quantity);
 		Bags.push_back(Bag2);
 
-		cout << "Enter Fruit Name";
+		cout << "Enter Fruit Name: ";
 				cin >> choice;
-			cout << "Enter Total Weight";
+			cout << "Enter Total Weight: ";
 				cin >> quantity;
+
+				cout << ""<<endl;
 
 			Fruits Bag3(choice, quantity);
 			Bags.push_back(Bag3);
@@ -45,10 +57,12 @@ cout << "Bags needed four (4)"<<endl;
 				cout << "Enter Total Weight: ";
 					cin >> quantity;
 
+					cout << ""<<endl;
+
 				Fruits Bag4(choice, quantity);
 				Bags.push_back(Bag4);
 
-				cout<<"Fruits:"<<endl;
+				cout<<"Fruits:"<<endl<<endl;
 
 				cout << "Fruit Name: "<<Bag1.getName()<< "	Weight: "<<Bag1.getWeight()<<endl;
 				cout << "\n";
@@ -61,5 +75,43 @@ cout << "Bags needed four (4)"<<endl;
 
 				cout << "Fruit Name: "<<Bag4.getName()<< "	Weight: "<<Bag4.getWeight()<<endl;
 
-				}
+cout << string(50, '\n');//clear screen
 
+cout<<"Enter a class attribute to search for: ";
+
+   cin>>search_key;
+
+    while(search_key != "#")//perform searches until sentinel entered
+    {
+        result = linearSearch(Bags, search_key);
+
+        cout<<"  \""<<search_key<<"\" was ";
+
+        if (result == -1)
+          cout<<"not found";
+        else
+          cout<<"found at index "<<result;
+
+
+        cout<<endl<<endl<<"Enter a class attribute to search for: ";
+        cin>>search_key;
+    }
+
+}
+
+int linearSearch(auto data, auto key)
+{
+
+	cout<< "Search Key is \""<<key<<"\""<<endl;
+
+		for (int i = 0; i < data.size(); i++)
+	{
+		if (data[i].getName == key)
+		{
+			return i;
+		}
+	}
+
+	return -1;
+
+}
