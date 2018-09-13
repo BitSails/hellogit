547a6a96dc42baee7b557cb48504f2d34b4d05b4
combermere246 <42950600+combermere246@users.noreply.github.com>
2018-09-13 19:37:36 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,34 +4,40 @@
 
 using namespace std;
 
+int main()
+
 {
-	
-	vector<int> v1;
-	vector<int> v2={5,8,22};
-	v1.push_back(17);
-	cout << "V2 has " << v2.size() << "elements" << endl;
-	
-	for (auto x:v2)
-	{
-		cout << x << endl;
-	}
-	
-	Data d0;
-	Data d1 = {7,24.83};
-	d0.setX(15);
-	d0.setY(3.14);
-	
-	vector<Data> v;
-	v.push_back( {7,21.01} );
-	v.push_back(d);
-	d.setX(10);
-	d.setY(12.10);
-	v.push_back(d);
-	
-	for (auto a:v)
-	{
-		cout << a.getx() << " ";
-		cout << a.getY() << endl;
-	return 0;
-	
+
+vector<int> v1;
+vector<int> v2={5,8,22};
+v1.push_back(17);
+cout << "V2 has " << v2.size() << " elements. They are:" << endl<<endl;
+
+for (auto x:v2)
+{
+cout << x << endl;
+}
+
+Data d0;
+Data d1 = {7,24.83};
+d0.setX(15);
+d0.setY(3.14);
+
+vector<Data> v;
+v.push_back( {7,21.01} );
+Data d;
+v.push_back(d);
+d.setX(10);
+d.setY(12.10);
+v.push_back(d);
+
+cout <<endl<<"The Vector V of type Data contains: "<< endl<<endl;
+
+for (auto a:v)
+{
+cout << a.getX() << " ";
+cout << a.getY() << endl<< endl;
+}
+return 0;
+
 }
