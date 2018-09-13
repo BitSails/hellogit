e57a08c9ffce847d8038c8df48bbf52641f32577
combermere246 <42950600+combermere246@users.noreply.github.com>
2018-09-13 19:36:36 -0300

    Update Data.h

--- Data.h
+++ Data.h
@@ -8,21 +8,21 @@ using namespace std;
 
 class Data
 {
-	private:
-	
-	int x;
-	float y;
-	
-	public:
-	
-	Data();
-	Data(int a, float b);
-	float getY();
-	int getX();
-	void setX(int a);
-	void setY(float a);
-	
-	
+private:
+
+int x;
+float y;
+
+public:
+
+Data();
+Data(int a, float b);
+float getY();
+int getX();
+void setX(int a);
+void setY(float a);
+
+
 };
 
 #endif
