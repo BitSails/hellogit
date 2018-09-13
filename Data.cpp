e3f298655ae70f88db14b458156288dfb98918c0
combermere246 <combermere246@users.noreply.github.com>
2018-09-13 19:35:36 -0400

    Data.h

--- Data.cpp
+++ Data.cpp
@@ -3,37 +3,6 @@
 
 using namespace std;
 
-<<<<<<< HEAD
-	
-	Data::Data()
-	{ 
-		x=0;
-		y=0.0;
-	}
-	
-	Data::Data(int a, float b)
-	{
-		x=a;
-		y=b;
-	}
-	float Data::getY()
-	{
-		return y;
-	}
-	int Data::getX()
-	{
-		return x;
-	}
-	void Data::setX(int a)
-	{
-		x=a;
-	}
-	void Data::setY(float a)
-	{
-		y=a;
-	}
-	
-=======
 
 Data::Data()
 { 
@@ -62,4 +31,4 @@ void Data::setY(float a)
 {
 y=a;
 }
->>>>>>> 547a6a96dc42baee7b557cb48504f2d34b4d05b4
+
