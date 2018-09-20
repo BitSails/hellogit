d664f2dde2c07bc71acd5dfb9a347710054002cd
combermere246 <combermere246@users.noreply.github.com>
2018-09-20 14:22:38 -0400

    Data.h

--- main.cpp
+++ main.cpp
@@ -12,6 +12,13 @@ void print (auto A)
 	cout<<endl;
 }
 	
+void printClass (auto A)
+{
+	for (auto a : A)
+	cout << a.getX() << " "<< a.getY() << " ";
+	
+	cout<<endl<<endl;
+}
 
 void insertionSort (auto& D)
 {
@@ -28,6 +35,21 @@ void insertionSort (auto& D)
 	}
 }
 
+void insertionSortClass (auto& D)
+{
+	for (int i=1; i < D.size(); i++)
+	{
+		int j=i;
+		while (j > 0 and D[j].getX() < D[j-1].getX())
+		{
+			swap (D[j], D[j-1]);
+			j--;
+		}
+		printClass(D);
+		
+	}
+}
+
 int main()
 
 
@@ -37,7 +59,7 @@ int main()
 	vector<int> v2={22,8,7,9,45};
 	v1.push_back(17);
 	
-	vector <int> w = {7, 4, 2, 19, 31, 11};
+	vector <int> w = {7, 6, 5, 4, 3, 2};
 	
 	insertionSort(w);
 	
@@ -67,8 +89,11 @@ int main()
 	v.push_back( {7,21.01} );
 	Data d;
 	v.push_back(d);
-	d.setX(10);
+	d.setX(6);
 	d.setY(12.10);
+	v.push_back(d);
+	d.setX(5);
+	d.setY(6.12);
 	v.push_back(d);
 	
 	cout <<endl<<"The Vector V of type Data contains: "<< endl<<endl;
@@ -80,8 +105,9 @@ int main()
 		cout << a.getX() << " ";
 		cout << a.getY() << endl<< endl;
 		
-		//insertionSort(v);
 	}
+	
+	insertionSortClass(v);
 	return 0;
 	
 }
